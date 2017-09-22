8170257c3177905ea2e9adff67b00b545ca0db33
dseredyn <dawid.seredynski@gmail.com>
2017-09-22 10:59:18 +0200

    support for install

--- src/planner.cpp
+++ src/planner.cpp
@@ -56,7 +56,7 @@
 #include "Eigen/LU"
 
 #include <collision_convex_model/collision_convex_model.h>
-#include "kin_model/kin_model.h"
+#include "kin_dyn_model/kin_model.h"
 #include "planer_utils/marker_publisher.h"
 #include "planer_utils/random_uniform.h"
 #include "planer_utils/utilities.h"
